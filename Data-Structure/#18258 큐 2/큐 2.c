9d021a75e6c43ce391fe0eab30de8ae037e94076
ryoo316 <123096470+ryoo316@users.noreply.github.com>
2023-02-15 16:42:34 +0900

    [Silver IV 18258] Title: 큐 2, Time: 564 ms, Memory: 8928 KB -NKLCBHub

---
+++ Data-Structure/#18258 큐 2/큐 2.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<string.h>
+int front = -1;
+int rear = -1;
+int cnt = 0;
+int queue[2000001] = {};
+void push(int v)
+{
+  rear++;
+  cnt++;
+  queue[rear] = v;
+}
+int empty()
+{
+  if(front == rear)
+    return 1;
+  else 
+    return 0;
+}
+int pop()
+{
+  if(empty() == 1){
+    printf("-1\n");
+  }
+  else{
+    front++;
+    cnt--;
+    printf("%d\n",queue[front]);
+    return queue[front];
+  }
+  return 0;
+}
+void f()
+{
+  if(cnt == 0) printf("-1\n");
+  else printf("%d\n",queue[front+1]);
+}
+void back()
+{
+  if(cnt == 0) printf("-1\n");
+  else printf("%d\n",queue[rear]);
+}
+void size(){
+  printf("%d\n",cnt);
+}
+int main()
+{
+  int n;
+  scanf("%d",&n);
+  getchar();
+  for(int t=0;t<n;t++){
+    char command[10] = {};
+    scanf("%s",command);
+    
+    if(strcmp("push", command) == 0){
+      int n;
+      scanf("%d", &n);
+      getchar();
+      push(n);
+    }
+    else if(command[0] == 'p'){
+      pop();
+    }
+    else if(command[0] == 'f'){
+      f();
+    }
+    else if(command[0] == 's'){
+      size();
+    }
+    else if(command[0] == 'e'){
+      printf("%d\n",empty());
+    }
+    else if(command[0] == 'b'){
+      back();
+    }
+  }
+}
